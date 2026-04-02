58319215c790c28b50368d4a1aaa81d31136043c
Ashrithhn <ashrith.sringeri@gmail.com>
2026-04-02 13:06:46 +0530

    js-scope

---
+++ defined.js
@@ -0,0 +1,8 @@
+var a;
+console.log(a);
+
+var a = 10;
+console.log(a);
+
+var a = "namste world";
+console.log(a);
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var x = 7;
- function getName() {
-     console.log("Namaste JavaScript");
-    }
+var x = 1000;
+a();
+b();
+console.log(x); // Output: 1000
 
- getName();
-//  console.log(getName);
+function a() {
+    var x = 10;
+    console.log(x); // Output: 10
+}
+
+function b() {
+    var x = 100;
+    console.log(x);
+}
