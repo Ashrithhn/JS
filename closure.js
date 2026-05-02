91004708d07c36ba9d4bc63bcabfae34e13a6963
Ashrithhn <ashrith.sringeri@gmail.com>
2026-05-02 13:18:26 +0530

    let and var in loops and timout

--- closure.js
+++ closure.js
@@ -1,15 +1,9 @@
-function createWallet(startingMoney) {
-  var money = startingMoney;
-  
-  return {
-    add: function(amount) { money += amount; },
-    spend: function(amount) { money -= amount; },
-    balance: function() { return money; }
-  }
+function x(){
+    var a = 10;
+    function y(){
+        console.log(a);
+    }
+y();
 }
 
-var myWallet = createWallet(500);
-myWallet.add(200);
-myWallet.spend(100);
-console.log(myWallet.balance()); // 600
-console.log(myWallet.money);     // undefined - try it!
+x();
